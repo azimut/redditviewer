e9d3441a592694ff138e32a457dd1cec357375b5
azimut <azimut.github@protonmail.com>
2020-08-15 06:20:38 -0300

    minor

--- main.go
+++ main.go
@@ -9,24 +9,22 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-// TODO: polymorfism? for hostname
-// TODO: validate that url.Path is a comment url
-// TODO: free json string?
-
 func main() {
-	//dat, err := ioutil.ReadFile("/home/sendai/.json.2")
 	var timeout int
 	flag.IntVar(&timeout, "t", 5, "timeout after seconds")
 	flag.Parse()
-	dat, err := request.GetFromParam(timeout)
+
+	data, err := request.GetFromParam(timeout)
 	if err != nil {
 		panic(err)
 	}
-	post := gjson.Get(dat, "0.data.children.0.data")
+
+	post := gjson.Get(data, "0.data.children.0.data")
 	printer.Print_Header(post)
+
 	num_comments := post.Get("num_comments").Int()
 	if num_comments > 0 {
-		comments := gjson.Get(dat, "1.data.children.#.data")
+		comments := gjson.Get(data, "1.data.children.#.data")
 		printer.Print_Posts(comments)
 	}
 }
